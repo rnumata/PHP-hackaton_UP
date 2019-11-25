2cb12f353cef05bf3908a7cb4a63f00f293f1919
rnumata <regis.numata@gmail.com>
2019-11-25 14:46:50 -0300

    Add files via upload

---
+++ hackaton/anagrama.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+
+</head>
+<body>
+    <div>
+        <form method="post" action="anagrama.php">
+            <div>
+                <label for="texto">Texto</label>
+                <input type="text" id="texto" name="texto" maxlength="16" pattern="[A-Z\s]+$" />
+            </div></br>
+
+            <div>
+                <button type="submit">Gerar Anagrama</button>
+            </div>
+        </form>
+    </div>
+
+<?php
+
+    /* Função file lê arquivo e retorna um array */
+    $f = file("lista.txt");
+
+    /* inicialização da variável */
+    $texto = "";
+    $lista = array();
+
+
+    if(empty($_POST)){
+
+    } else {
+        $texto = $_POST['texto'];
+        echo "Palavra digitada => ".$texto;
+        echo "</br>";
+
+        $anagrama = array();
+
+        /* Popula um array com o anagrama da variável $texto */
+        for($i=0; $i<10000; $i++){
+            array_push($anagrama, str_shuffle($texto));
+        }
+
+        /* Função array_unique remove valores duplicados de um array */
+        $lista = array_unique($anagrama);
+    }
+
+
+    /* foreach para comparar e imprimir os anagramas que constam no .txt */
+    echo "</br>";
+
+    echo "Anagrama que consta na lista: "."</br>";
+    foreach ($lista as $listaII){
+        foreach ($f as $fII){
+            if($listaII == trim($fII)){
+                echo $listaII." == ".trim(($fII));
+                echo "</br>";
+            }
+        }
+    }
+
+
+?>
+    <br>
+    <table>
+        <th>Anagrama</th>
+    </table>
+
+    <?php
+        foreach ($lista as $ana) {
+            ?>
+
+        <tr> <?=$ana?></tr>
+
+    <?php
+        echo "</br>";
+        }
+    ?>
+
+
+</body>
+</html>
